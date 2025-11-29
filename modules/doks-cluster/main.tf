47acbb91a4893d6f5808b61a1cd14e23bc0c0a73
Cédric Michaux <cedric@fullfrontend.be>
2025-11-29 11:42:17 +0100

    correct Auto Update Cluster time

--- modules/doks-cluster/main.tf
+++ modules/doks-cluster/main.tf
@@ -7,7 +7,7 @@ resource "digitalocean_kubernetes_cluster" "k8s" {
   version = data.digitalocean_kubernetes_versions.current.latest_version
 
   maintenance_policy {
-    start_time = "04:00"
+    start_time = "03:00"
     day        = "sunday"
   }
 
