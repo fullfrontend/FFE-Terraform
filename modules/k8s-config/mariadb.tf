ef5bd5cbe7efbe5e0d7310e9e61e92218f373e8c
Cédric Michaux <cedric@fullfrontend.be>
2025-12-03 19:15:15 +0100

    Hardened security for mariadb

--- modules/k8s-config/mariadb.tf
+++ modules/k8s-config/mariadb.tf
@@ -1,5 +1,5 @@
 locals {
-  mariadb_init_sql = length(var.mariadb_app_credentials) > 0 ? templatefile("${path.module}/templates/mariadb-init.sql.tmpl", { apps = var.mariadb_app_credentials }) : ""
+  mariadb_init_creds = length(var.mariadb_app_credentials) > 0 ? join("\n", [for app in var.mariadb_app_credentials : "${app.db_name}:${app.user}:${app.password}"]) : ""
 }
 
 resource "kubernetes_secret" "mariadb" {
@@ -13,8 +13,8 @@ resource "kubernetes_secret" "mariadb" {
   }
 }
 
-resource "kubernetes_config_map" "mariadb_init" {
-  count = local.mariadb_init_sql != "" ? 1 : 0
+resource "kubernetes_secret" "mariadb_init" {
+  count = local.mariadb_init_creds != "" ? 1 : 0
 
   metadata {
     name      = "mariadb-initdb"
@@ -22,7 +22,7 @@ resource "kubernetes_config_map" "mariadb_init" {
   }
 
   data = {
-    "init.sql" = local.mariadb_init_sql
+    creds = local.mariadb_init_creds
   }
 }
 
@@ -98,14 +98,6 @@ resource "kubernetes_stateful_set_v1" "mariadb" {
             name       = "mariadb-data"
             mount_path = "/var/lib/mysql"
           }
-
-          dynamic "volume_mount" {
-            for_each = local.mariadb_init_sql != "" ? [1] : []
-            content {
-              name       = "init-sql"
-              mount_path = "/docker-entrypoint-initdb.d"
-            }
-          }
         }
 
         volume {
@@ -113,17 +105,6 @@ resource "kubernetes_stateful_set_v1" "mariadb" {
 
           persistent_volume_claim {
             claim_name = "mariadb-data"
-          }
-        }
-
-        dynamic "volume" {
-          for_each = local.mariadb_init_sql != "" ? [1] : []
-          content {
-            name = "init-sql"
-
-            config_map {
-              name = kubernetes_config_map.mariadb_init[0].metadata[0].name
-            }
           }
         }
       }
@@ -169,4 +150,102 @@ resource "kubernetes_secret" "mariadb_apps" {
     user     = each.value.user
     password = each.value.password
   }
+}
+
+resource "kubernetes_job" "mariadb_init" {
+  count = local.mariadb_init_creds != "" ? 1 : 0
+
+  metadata {
+    name      = "mariadb-init"
+    namespace = kubernetes_namespace.data.metadata[0].name
+    labels = {
+      app = "mariadb"
+      job = "mariadb-init"
+    }
+  }
+
+  spec {
+    backoff_limit              = 3
+    ttl_seconds_after_finished = 120
+
+    template {
+      metadata {
+        labels = {
+          app = "mariadb"
+          job = "mariadb-init"
+        }
+      }
+
+      spec {
+        restart_policy = "OnFailure"
+
+        container {
+          name  = "init"
+          image = var.mariadb_image
+
+          command = [
+            "/bin/sh",
+            "-c",
+            <<-EOF
+              set -e
+              export MYSQL_PWD="$MARIADB_ROOT_PASSWORD"
+              until mysqladmin ping -h "$MARIADB_HOST" -P "$MARIADB_PORT" -u "$MARIADB_ROOT_USER" --silent; do
+                echo "Waiting for mariadb at $MARIADB_HOST:$MARIADB_PORT"
+                sleep 2
+              done
+              while IFS=: read -r DB_NAME DB_USER DB_PASSWORD; do
+                mysql -h "$MARIADB_HOST" -P "$MARIADB_PORT" -u "$MARIADB_ROOT_USER" <<SQL
+CREATE DATABASE IF NOT EXISTS \`$DB_NAME\`;
+CREATE USER IF NOT EXISTS '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD';
+GRANT ALL PRIVILEGES ON \`$DB_NAME\`.* TO '$DB_USER'@'%';
+FLUSH PRIVILEGES;
+SQL
+              done </secrets/creds
+            EOF
+          ]
+
+          env {
+            name  = "MARIADB_HOST"
+            value = kubernetes_service.mariadb.metadata[0].name
+          }
+          env {
+            name  = "MARIADB_PORT"
+            value = "3306"
+          }
+          env {
+            name  = "MARIADB_ROOT_USER"
+            value = "root"
+          }
+          env {
+            name = "MARIADB_ROOT_PASSWORD"
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret.mariadb.metadata[0].name
+                key  = "MARIADB_ROOT_PASSWORD"
+              }
+            }
+          }
+
+          volume_mount {
+            name       = "init-creds"
+            mount_path = "/secrets"
+            read_only  = true
+          }
+        }
+
+        volume {
+          name = "init-creds"
+
+          secret {
+            secret_name = kubernetes_secret.mariadb_init[0].metadata[0].name
+          }
+        }
+      }
+    }
+  }
+
+  depends_on = [
+    kubernetes_service.mariadb,
+    kubernetes_secret.mariadb_init,
+  ]
 }
