d5d71d568fd9104276e102a785979a219c2ff81b
Cédric Michaux <cedric@fullfrontend.be>
2025-12-05 05:39:01 +0100

    Repository setup

--- modules/ffe-website/ingress.tf
+++ modules/ffe-website/ingress.tf
@@ -1,4 +1,6 @@
 locals {
+  wordpress_www_host = "www.${var.host}"
+
   wordpress_annotations_https = merge(
     {
       "kubernetes.io/ingress.class"                      = var.ingress_class_name
@@ -17,6 +19,27 @@ locals {
     "kubernetes.io/ingress.allow-http"                 = "true"
     "traefik.ingress.kubernetes.io/router.entrypoints" = "web"
     "traefik.ingress.kubernetes.io/router.middlewares" = "infra-redirect-https@kubernetescrd"
+  }
+}
+
+# Middleware to redirect www -> apex over HTTPS
+resource "kubernetes_manifest" "wordpress_www_redirect" {
+  count      = var.enable_tls ? 1 : 0
+
+  manifest = {
+    apiVersion = "traefik.io/v1alpha1"
+    kind       = "Middleware"
+    metadata = {
+      name      = "wordpress-www-redirect"
+      namespace = kubernetes_namespace.wordpress.metadata[0].name
+    }
+    spec = {
+      redirectRegex = {
+        regex       = "https?://www\\.${var.host}/?(.*)"
+        replacement = "https://${var.host}/$1"
+        permanent   = true
+      }
+    }
   }
 }
 
@@ -73,6 +96,44 @@ resource "kubernetes_ingress_v1" "wordpress_http_redirect" {
 
     rule {
       host = var.host
+      http {
+        path {
+          path      = "/"
+          path_type = "Prefix"
+          backend {
+            service {
+              name = kubernetes_service.wordpress.metadata[0].name
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+# HTTP-only ingress to catch www and redirect to apex
+resource "kubernetes_ingress_v1" "wordpress_www_redirect" {
+  count = var.enable_tls ? 1 : 0
+
+  metadata {
+    name        = "wordpress-www-http"
+    namespace   = kubernetes_namespace.wordpress.metadata[0].name
+    annotations = {
+      "kubernetes.io/ingress.class"                      = var.ingress_class_name
+      "kubernetes.io/ingress.allow-http"                 = "true"
+      "traefik.ingress.kubernetes.io/router.entrypoints" = "web"
+      "traefik.ingress.kubernetes.io/router.middlewares" = "${kubernetes_namespace.wordpress.metadata[0].name}-wordpress-www-redirect@kubernetescrd"
+    }
+  }
+
+  spec {
+    ingress_class_name = var.ingress_class_name
+
+    rule {
+      host = local.wordpress_www_host
       http {
         path {
           path      = "/"
