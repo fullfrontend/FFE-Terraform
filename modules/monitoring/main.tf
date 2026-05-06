97d18155f7474a40d32a0430b1b09cce60dfba83
Cédric Michaux <cedric@fullfrontend.be>
2026-05-06 19:41:16 +0200

    chore(monitoring): pin kube-prometheus-stack to 84.5.0

--- modules/monitoring/main.tf
+++ modules/monitoring/main.tf
@@ -9,6 +9,7 @@ resource "helm_release" "kube_prometheus_stack" {
   namespace        = kubernetes_namespace.monitoring.metadata[0].name
   repository       = "https://prometheus-community.github.io/helm-charts"
   chart            = "kube-prometheus-stack"
+  version          = "84.5.0"
   cleanup_on_fail  = true
   atomic           = true
   create_namespace = false
